2d52cf8df920bb8051954e505b4dfba7a93dc1a3
SoftNova Tech <softnovatech26@gmail.com>
2026-01-29 20:36:28 +0500

    Footer Mt resolve

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-blue-600 text-white text-center w-full h-40 flex items-center justify-center mt-10">
+    <footer className="bg-blue-600 text-white text-center w-full h-40 flex items-center justify-center mt-">
       © 2026 SoftNova Tech. All rights reserved.
     </footer>
   );
