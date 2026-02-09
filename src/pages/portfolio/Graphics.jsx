2205e966339187fdaffb67b61df0ca21309ccfec
unknown <muhammadmairaj123@gmail.com>
2026-02-09 03:02:20 -0800

    Task Porfolio pgs

--- src/pages/portfolio/Graphics.jsx
+++ src/pages/portfolio/Graphics.jsx
@@ -1,25 +1,22 @@
 const Graphics = () => {
   return (
     <div className="min-h-screen bg-gray-100 py-20 px-6">
-      <h1 className="text-4xl font-bold text-center mb-12">
-        Graphics Designing
+
+      {/* MAIN HEADING */}
+      <h1 className="text-6xl font-bold text-center mb-12 font-serif">
+        Graphic Designing
       </h1>
 
-      <div className="grid md:grid-cols-3 gap-8 max-w-6xl mx-auto">
-        <div className="bg-white shadow-xl p-6 rounded-xl">
-          <h3 className="font-bold text-xl">Logo Design</h3>
-          <p className="text-gray-600 mt-2">
-            Brand identity & logo
-          </p>
-        </div>
-
-        <div className="bg-white shadow-xl p-6 rounded-xl">
-          <h3 className="font-bold text-xl">Social Media Post</h3>
-          <p className="text-gray-600 mt-2">
-            Instagram & Facebook creatives
-          </p>
-        </div>
+      {/* PARAGRAPH */}
+      <div>
+      <h2 className="text-10x1 font-bold text-center text-gray-600 mb-10 font-serif">
+        We've Done Lot's Of Work <br />
+        Let's check Some From Here
+      </h2>
       </div>
+
+      
+
     </div>
   );
 };
