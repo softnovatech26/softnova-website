58fcdd7a9d2bdedbf62fe4daeae3e08828d4574f
Shahnaz Aqsa <shahnazaqsabscs19@gmail.com>
2026-02-25 23:16:50 +0500

    connected ports

--- website_backend/website_backend/settings.py
+++ website_backend/website_backend/settings.py
@@ -93,8 +93,10 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 # CORS settings
+ALLOWED_HOSTS = ["https://softnova-website.onrender.com/"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://softnova-weld.vercel.app/",
 ]
 CORS_ALLOW_CREDENTIALS = True
