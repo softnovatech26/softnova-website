22cafe06fc3cca979ccfef0e684c01f6508d15ad
Shahnaz Aqsa <shahnazaqsabscs19@gmail.com>
2026-02-25 23:53:21 +0500

    added configurations

--- website_backend/website_backend/settings.py
+++ website_backend/website_backend/settings.py
@@ -93,7 +93,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 # CORS settings
-ALLOWED_HOSTS = ["https://softnova-website.onrender.com/"]
+ALLOWED_HOSTS = ["softnova-website.onrender.com"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
