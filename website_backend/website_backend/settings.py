7529178ae14d61ae217a55e3cc1601951c063342
Shahnaz Aqsa <shahnazaqsabscs19@gmail.com>
2026-02-27 19:34:48 +0500

    Updated settings.py

--- website_backend/website_backend/settings.py
+++ website_backend/website_backend/settings.py
@@ -62,9 +62,9 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+# DATABASES = {
+#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
